c211323dfcf71ccf5372fe4b8fd264766469ee5a
Phuc Nguyen <phuc001133@gmail.com>
2022-12-31 17:07:03 +0700

    Update LoginPage.java

--- src/test/java/page/LoginPage.java
+++ src/test/java/page/LoginPage.java
@@ -1,9 +1,17 @@
 package page;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 
 public class LoginPage extends BasePage{
     public LoginPage(WebDriver driver) {
         super(driver);
     }
+
+    @FindBy(how = How.CSS ,using = "[name=username]")
+    private WebElement txtUsername;
+
+    
 }
